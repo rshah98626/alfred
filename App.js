2dac32217f12748e066d96bd36e447507be6bd4f
Rahul Shah <rshah98626@gmail.com>
2020-12-22 19:15:34 -0600

    added start up hook

--- App.js
+++ App.js
@@ -1,37 +1,46 @@
-import React from 'react';
-import { Provider, useSelector } from 'react-redux';
-import LoginView from './src/LoginView';
-import { store } from './src/store';
+import React, { useEffect } from 'react'
+import { Provider, useSelector } from 'react-redux'
+import LoginView from './src/LoginView'
+import { store } from './src/store'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import SplashScreen from './src/SplashScreen'
 
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
 
 const App = () => {
-  const isAuthenticated = useSelector(state => state.login.isAuthenticated)
-  const stillLoading = useSelector(state => state.login.stillLoading)
-  const errorMessage = useSelector(state => state.login.errorMessage)
-  
+  const isAuthenticated = useSelector((state) => state.login.isAuthenticated)
+  const stillLoading = useSelector((state) => state.login.stillLoading)
+  const errorMessage = useSelector((state) => state.login.errorMessage)
+
   if (stillLoading) {
-    console.log("LOADING")
+    console.log('LOADING')
   }
   if (isAuthenticated) {
-    console.log("WE GOT THE RIGHT STATE")
+    console.log('WE GOT THE RIGHT STATE')
   }
   if (errorMessage) {
     console.log(errorMessage)
-    console.log("FAILED LOGIN")
+    console.log('FAILED LOGIN')
   }
   return (
-    <LoginView />
-  );
+    <NavigationContainer>
+      <LoginView />
+    </NavigationContainer>
+  )
 }
 
 const Root = () => {
+  useEffect(() => console.log('INITIALIZED APP'), [])
   return (
     <Provider store={store}>
       {/*<PersistGate loading={null} persistor={persistor}>*/}
-        <App />
+      <App />
       {/*</PersistGate>*/}
     </Provider>
-  );
+  )
 }
 
 export default Root
